3a37520f7c5264d26417f901d4f839b9c53eb89e
Dalitso Mlota <dalimlota22@gmail.com>
2025-11-28 06:40:40 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<!--[if lte IE 9]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 9]><!-->
+<html class="no-js">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<title>Parteners In Hope - Home</title>
+<!--=================================
+Meta tags
+=================================-->
+<meta name="description" content="">
+<meta content="yes" name="apple-mobile-web-app-capable" />
+<meta name="viewport" content="minimum-scale=1.0, width=device-width, maximum-scale=1, user-scalable=no" />
+
+<!--=================================
+Style Sheets
+=================================-->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>  
+    
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="assets/css/flexslider.css">
+<link rel="stylesheet" type="text/css" href="assets/css/animations.css">
+<link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="assets/css/jquery.flickr.css">
+<link rel="stylesheet" type="text/css" href="assets/css/prettyPhoto.css">
+<link rel="stylesheet" href="assets/css/main.css">
+
+<script src="assets/js/lib/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+</head>
+<body>
+
+<!--========================================
+Body Content
+===========================================-->
+<div class="offsetWrap">
+    
+    <div class="offsetMaker"> 
+        
+        <header class="bg-white doc-header">
+            <div class="head-contact clearfix">
+            <div class="container">
+                <ul class="nav-top pull-left">
+                    <li><a href="patient/" class="appointment">Book Appointment Now</a></li>
+                </ul>
+
+                <ul class="contact pull-right">
+                    <li><a href="#"><i class="fa fa-phone"></i>0800-1234-567</a></li>
+                    <li><a href="#"><i class="fa fa-envelope"></i>info@partnersinhope.com</a></li>
+                </ul>
+
+            </div>
+            </div>
+
+            
+            <nav id="sticktop">
+            <div class="container">
+                
+                    <a href="index.html" class="text-left logo"><img width="150px" src="logo.png" alt=""></a>
+                    <ul class="socials">
+                        <li><a href="#" class="fb"><i class="fa fa-facebook-f"></i></a></li>
+                        <li><a href="#" class="twitter"><i class="fa fa-twitter"></i></a></li>
+                    </ul>
+                    <a href="#" class="nav-triger"><span class="fa fa-navicon"></span></a>
+                    <ul class="main-menu">
+                        <li class="active"><a href="index.php">Home</a></li>
+                        <li><a href="admin/">Admin Login</a></li>
+                        <li><a href="patient/">Patient Login</a></li>
+                        <li><a href="doctor/">Doctor Login</a></li>
+                        <li><a href="receptionist/">Receptionist Login</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>    
+
+        <section class="custom-slider no-pad">
+            <div id="home-slider" class="xv_slider flexslider">
+                <ul class="slides">
+                    <li class="xv_slide" data-slideBg="url(woman-2141808_1280.jpg)" >
+                        <div class="flex-caption">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-offset-7 col-md-5 col-sm-6 col-sm-offset-6">
+                                    <h3 class="mb-25 text-uppercase color-white text-bold after-left-l pb-30 animated fadeInLeft">
+    Expert Healthcare Professionals
+</h3>
+<p class="animated fadeInRight">
+    We are committed to strengthening healthcare infrastructure in Malawi. Our dedicated team of doctors and healthcare workers provide compassionate care, ensuring that all patients receive the attention they deserve. We are available <strong>7 days a week</strong> to serve our community.
+</p>
+      <a href="patient/" class="btn animated fadeInUp">Take Appoitments</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="xv_slide" data-slideBg="url(woman-2141808_1280.jpg)" >
+                        <div class="flex-caption">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-offset-7 col-md-5 col-sm-6 col-sm-offset-6">
+                                    <h3 class="mb-25 text-uppercase color-white text-bold after-left-l pb-30 animated fadeInLeft">
+    Expert Healthcare Providers
+</h3>
+<p class="animated fadeInRight">
+    At Partners in Hope, we are dedicated to improving healthcare access and strengthening healthcare systems in Malawi. Our team of experienced doctors and healthcare professionals is committed to delivering quality care, and we are available <strong>7 days a week</strong> to support those in need.
+</p>
+      <a href="patient/" class="btn animated fadeInUp">Take Appoitments</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+
+                    
+                    </li>
+                </ul>
+            </div>
+        </section>
+
+    
+
+<script src="assets/js/lib/jquery.js"></script>
+<script src="assets/js/lib/css3-animate-it.js"></script>
+<script src="assets/js/lib/jquery.flexslider-min.js"></script>
+<script src="assets/js/lib/jquery.sticky.js"></script>
+<script src="assets/js/lib/jquery.waitforimages.js"></script>
+<script src="assets/js/lib/jflickrfeed.min.js"></script>
+<script src="assets/js/lib/jquery.prettyPhoto.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+<script src="assets/js/app/main.js"></script>
+
+</body>
+</html>
